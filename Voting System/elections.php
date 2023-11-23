cfdfc8c2f08f02d29d98ac04b32a1ed21153358a
Kio Salonga <92920744+KioKiks@users.noreply.github.com>
2023-11-23 18:11:00 +0800

    Add files via upload

---
+++ Voting System/elections.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="elections.css">
+    <title>Voting System</title>
+</head>
+<body>
+    <div class="header">
+        <div class="logo">VOTING SYSTEM</div>
+        <div class="profile-section">
+            <div class="welcome-text">Welcome, User!</div>
+            <div class="profile-picture"></div>
+            <a href="firtpage.php" class="sign-out-btn">SIGN OUT</a>
+        </div>
+    </div>
+        <div class="election-text">
+            <p>ELECTIONS</p>
+    </div>
+        <button type='submit' class="vote-btn">VOTE</button>
+            <div class="footer">
+    </div>
+</body>
+</html>
+
+
+
+
