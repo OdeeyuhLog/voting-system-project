e62aaae2e2e7a46ecf3bb1bb446ad13984b7e47d
Kio Salonga <92920744+KioKiks@users.noreply.github.com>
2023-12-09 23:38:36 +0800

    Add files via upload

---
+++ components/tally.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="compilecss/admin_dashboard.css">
+    <link rel="stylesheet" href="styles.css"> <!-- Make sure to link your CSS file here -->
+    <title>Document</title>
+</head>
+<body>
+
+    <div class="row">
+        <div class="column left">
+            <h2>Admin Dashboard</h2>
+    
+            <div class="links">
+                <a href="">
+                    <img src="./image/list-search.png" height="20px" width="20px" alt="" style="margin-right: 10px;">
+                    Overview
+                </a>
+                <a href="">
+                    <img src="./image/group.png" height="20px" width="20px" alt="" style="margin-right: 10px;">
+                    Users
+                </a>
+                <a href="">
+                    <img src="./image/bar-chart.png" height="20px" width="20px" alt="" style="margin-right: 10px;">
+                    Statistics
+                </a>
+                <a href="">
+                    <img src="./image/candidate.png" height="20px" width="20px" alt="" style="margin-right: 10px;">
+                    Candidates
+                </a>
+            </div>
+        </div>
+
+        <div class="welcome">
+            <h2>Welcome, Admin!</h2>
+        </div>
+    </div>
+
+        <div class="rectangle">
+            <div class="voting-countdown-text">Voting countdown:</div>
+                <img src="./image/clock.png" alt="Clock Image" class="clock-image">
+            <div class="countdown">3 DAYS, 2 HOURS, 50 MINUTES</div>
+            </div>
+        </div>
+
+         <!-- Rectangle 5 -->
+        <div class="rectangle-5">
+        <!-- User Count -->
+            <div class="user-count">
+                <p>User Count</p>
+        </div>
+
+        <!-- 200 -->
+        <div class="number-200">
+            <p>200</p>
+        </div>
+    </div>
+
+         <!-- Rectangle 4 -->
+    <div class="rectangle-4">
+        <!-- Number of Candidates -->
+        <p class="candidates-count">Number of Candidates</p>
+
+        <!-- 33 -->
+        <div class="number-33">
+            <p>33</p>
+        </div>
+    </div>
+
+     <!-- Rectangle 6 -->
+    <div class="rectangle-6">
+        <!-- Voted vs Unvoted -->
+        <p class="voted-vs-unvoted">Voted vs Unvoted</p>
+
+        <!-- Circle Image -->
+        <img src="./image/circle.png" alt="Circle Image" class="circle-image">
+    </div>
+
+    <div class="sign-out-btn"></div>
+        <a href="index.php" class="sign-out-btn">SIGN OUT</a>
+        </div>
+    </div>
+
+</body>
+</html>
